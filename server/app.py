88d8b2dd2ec813c842eede1bd8fc58774004684d
betalantz <betalantz@gmail.com>
2023-08-18 00:31:41 -0700

    add to README and first 2 routes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 from models import Cheese, Producer, db
-
-# from flask_restful import Api, Resource
-
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -16,13 +14,33 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = Api(app)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    response = make_response({"message": "Hello Fromagers!"}, 200)
-    return response
+# @app.route("/")
+# def index():
+#     response = make_response({"message": "Hello Fromagers!"}, 200)
+#     return response
+
+
+class Producers(Resource):
+    def get(self):
+        producers = [
+            producer.to_dict(rules=("-cheeses",)) for producer in Producer.query.all()
+        ]
+        response = make_response(jsonify(producers), 200)
+        return response
+
+
+class ProducersById(Resource):
+    def get(self, id):
+        producer = Producer.query.get_or_404(id)
+        response = make_response(jsonify(producer.to_dict()), 200)
+        return response
+
+
+api.add_resource(Producers, "/producers")
+api.add_resource(ProducersById, "/producers/<int:id>")
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -6,10 +6,17 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
 
-class Producer(db.Model, SerializerMixin):
+class TimestampMixin:
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now()
+    )
+
+
+class Producer(db.Model, SerializerMixin, TimestampMixin):
     __tablename__ = "producers"
 
-    serialize_rules = ("-cheeses.producer",)
+    serialize_rules = ("-cheeses.producer", "-created_at", "-updated_at")
 
     id = db.Column(db.Integer, primary_key=True)
     founding_year = db.Column(db.Integer)
@@ -24,10 +31,10 @@ class Producer(db.Model, SerializerMixin):
         return f"<Producer {self.id} Name: {self.name}>"
 
 
-class Cheese(db.Model, SerializerMixin):
+class Cheese(db.Model, SerializerMixin, TimestampMixin):
     __tablename__ = "cheeses"
 
-    serialize_rules = ("-producer.cheeses",)
+    serialize_rules = ("-producer.cheeses", "-created_at", "-updated_at")
 
     id = db.Column(db.Integer, primary_key=True)
     kind = db.Column(db.String)
